3642e61a0ea5d1480a75107c7db3fe49c6bf2051
Petro <petro@petro.ws>
2020-02-08 22:22:45 +0100

    zigbee2mqtt ikea lamps working

---
+++ transform/z2m-command-state.js
@@ -0,0 +1,9 @@
+(function(x) {
+    var result = "";
+    if (x == '1' || x == 'ON') {
+        result="{ \"state\": \"ON\" }";
+    } else {
+        result="{ \"state\": \"OFF\" }";
+    }
+    return result;
+})(input)
