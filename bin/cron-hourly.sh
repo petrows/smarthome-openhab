c5962e3de15407cd739096a5e170264a2b6b6c67
Petro <petro@petro.ws>
2020-02-18 21:58:09 +0100

    Repo cleanup

--- bin/cron-hourly.sh
+++ bin/cron-hourly.sh
@@ -2,10 +2,7 @@
 
 ROOT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-MQTT_HOST="localhost"
-MQTT_PORT="1883"
-MQTT_USER="openhabian"
-MQTT_PASS="***REMOVED***"
+source "$ROOT_PATH/config.sh"
 
 # Call radiation
 $ROOT_PATH/odl-parse.py --mqtt-host $MQTT_HOST --mqtt-port $MQTT_PORT --mqtt-user $MQTT_USER --mqtt-pass $MQTT_PASS --mqtt-topic weather/radiation https://odlinfo.bfs.de/DE/aktuelles/messstelle/082151090.html
