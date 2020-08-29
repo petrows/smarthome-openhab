7754d794b9c1991d78670e8b26791b9b7ca8b014
Peter Golovachev <petro@petro.ws>
2020-08-29 17:05:38 +0200

    Openhab hourly timer service

--- bin/odl-parse.py
+++ bin/odl-parse.py
@@ -4,6 +4,7 @@ from lxml import html
 import paho.mqtt.publish as publish
 import requests
 import argparse
+import logging
 
 rad_level = 0.0136
 
@@ -12,15 +13,22 @@ parser = argparse.ArgumentParser(description='Parse radiation level')
 parser.add_argument('url', metavar='URL', type=str,
                     help='URL to the odlinfo.bfs.de page, e.q. https://odlinfo.bfs.de/DE/aktuelles/messstelle/082151090.html')
 
-parser.add_argument('--mqtt-host', help='MQTT broker host')
-parser.add_argument('--mqtt-port', help='MQTT broker port')
-parser.add_argument('--mqtt-user', help='MQTT broker user')
-parser.add_argument('--mqtt-pass', help='MQTT broker password')
-parser.add_argument('--mqtt-topic', help='MQTT broker topic')
+parser.add_argument(
+    '--log', help='Log level: DEBUG, INFO, WARNING, ERROR, CRITICAL', default='INFO')
+parser.add_argument('--mqtt-host', help='MQTT broker host', default='localhost')
+parser.add_argument('--mqtt-port', help='MQTT broker port', default='1883')
+parser.add_argument('--mqtt-user', help='MQTT broker user', default='user')
+parser.add_argument('--mqtt-pass', help='MQTT broker password', default='password')
+parser.add_argument('--mqtt-topic', help='MQTT broker topic', default='odl-radlevel')
 
 args = parser.parse_args()
 
-print(args)
+numeric_level = getattr(logging, args.log.upper(), None)
+if not isinstance(numeric_level, int):
+    raise ValueError('Invalid log level: %s' % numeric_level)
+logging.basicConfig(level=numeric_level)
+
+logging.debug(args)
 
 mqtt_auth = {'username':args.mqtt_user, 'password':args.mqtt_pass}
 
@@ -31,11 +39,11 @@ mes = tree.xpath('//p[@class="aktmw"]/strong/text()')
 mes = mes[0]
 mes = mes.split(' ')
 mes = mes[0]
-mes = mes.replace(',','.')
+mes = mes.replace(',', '.')
 
 mes = float(mes)
 
-print("Current value is: %f" % mes)
+logging.info("Radiation value is: %f" % mes)
 
 publish.single(args.mqtt_topic, payload=mes, qos=0, retain=True, hostname=args.mqtt_host,
-    port=int(args.mqtt_port), client_id="WeatherRadiationMeter", auth=mqtt_auth)
+               port=int(args.mqtt_port), client_id="WeatherRadiationMeter", auth=mqtt_auth)
