d64770fa96c3a1338ed03c9928658c03c579aa6a
Peter Golovachev <petro@petro.ws>
2020-12-06 22:13:52 +0100

    Add Zibgee-map script

---
+++ bin/zigbee-map.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# additional dependencies: graphviz imagemagick
+
+# put temporal files in ram filesystem
+file="/tmp/networkmap"
+fechahora=$(date '+%F-%H:%M')
+#~ echo $fechahora
+
+ROOT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+source "$ROOT_PATH/config.sh"
+
+mosquitto_sub -u $MQTT_USER -P $MQTT_PASS -t zigbee2mqtt/bridge/networkmap/graphviz -C 1 >${file}.dot &
+mosquitto_pub -u $MQTT_USER -P $MQTT_PASS -t zigbee2mqtt/bridge/networkmap -m graphviz
+
+# wait until mosquitto_sub ends
+wait
+
+# generate graphic with graphviz (change to short texts with sed)
+cat ${file}.dot|sed -e 's/Xiaomi Aqara temperature, humidity and pressure sensor/AqaraTHP/g'|sed -e 's/Xiaomi Mi\/Aqara smart home cube/AqaraCube/g'|sed -e 's/Xiaomi Aqaradouble key wireless wall switch/AqaraDoubleSwitch/g'|circo -Tsvg  > ${file}${fechahora}.svg
+
+# display with imageMagick command
+# display ${file}${fechahora}.svg &
+echo "File: ${file}${fechahora}.svg"
