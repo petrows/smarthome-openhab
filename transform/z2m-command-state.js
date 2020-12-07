e6363b55a876f1f2afb79ce4638f25d441dc7176
Peter Golovachev <petro@petro.ws>
2020-12-07 21:47:16 +0100

    Add 2nd Lager lamp for auto light, add default transition

--- transform/z2m-command-state.js
+++ transform/z2m-command-state.js
@@ -3,7 +3,7 @@
     if (x == '1' || x == 'ON') {
         result="{ \"state\": \"ON\" }";
     } else {
-        result="{ \"state\": \"OFF\" }";
+        result = "{ \"state\": \"OFF\", \"transition\": 2 }";
     }
     return result;
 })(input)
