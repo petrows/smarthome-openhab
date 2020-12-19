0c5d1eec9f4141a6a205f0269b236e0bca28ed45
Peter Golovachev <petro@petro.ws>
2020-12-19 21:39:58 +0100

    Ignore IDE trash folder

--- .Trash-1000/files/z2m-command-ct-mired.js
+++
@@ -1,9 +0,0 @@
-(function(x) {
-    var result = "";
-    if (x == '1' || x == 'ON') {
-        result="{ \"state\": \"ON\" }";
-    } else {
-        result = "{ \"state\": \"OFF\", \"transition\": 2 }";
-    }
-    return result;
-})(input)
