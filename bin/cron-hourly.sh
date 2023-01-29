c3db6fa835884421a40c70f56c11e46f096dc549
Peter Golovachev <petro@petro.ws>
2023-01-29 12:24:51 +0100

    Fix radiation level parsing - using API

--- bin/cron-hourly.sh
+++ bin/cron-hourly.sh
@@ -5,4 +5,4 @@ ROOT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$ROOT_PATH/config.sh"
 
 # Call radiation
-$ROOT_PATH/odl-parse.py --log=INFO --mqtt-host $MQTT_HOST --mqtt-port $MQTT_PORT --mqtt-user $MQTT_USER --mqtt-pass $MQTT_PASS --mqtt-topic weather/radiation https://odlinfo.bfs.de/DE/aktuelles/messstelle/082151090.html
+$ROOT_PATH/odl-parse.py --log=INFO --mqtt-host $MQTT_HOST --mqtt-port $MQTT_PORT --mqtt-user $MQTT_USER --mqtt-pass $MQTT_PASS --mqtt-topic weather/radiation 76297
--- bin/odl-parse.py
+++ bin/odl-parse.py
@@ -5,7 +5,7 @@ import paho.mqtt.publish as publish
 import requests
 import argparse
 import logging
-import pprint
+import sys
 
 rad_level = 0.0001
 
@@ -15,8 +15,8 @@ headers = {
 
 parser = argparse.ArgumentParser(description='Parse radiation level')
 
-parser.add_argument('url', metavar='URL', type=str,
-                    help='URL to the odlinfo.bfs.de page, e.q. https://odlinfo.bfs.de/DE/aktuelles/messstelle/082151090.html')
+parser.add_argument('plz', metavar='plz', type=int,
+                    help='POstcode of measurment place, eg 76133')
 
 parser.add_argument(
     '--log', help='Log level: DEBUG, INFO, WARNING, ERROR, CRITICAL', default='INFO')
@@ -37,18 +37,26 @@ logging.debug(args)
 
 mqtt_auth = {'username':args.mqtt_user, 'password':args.mqtt_pass}
 
-page = requests.get(args.url, headers=headers)
-tree = html.fromstring(page.content)
-mes = tree.xpath('//p[@class="aktmw"]/strong/text()')
-pprint.pp(mes)
-mes = mes[0]
-mes = mes.split(' ')
-mes = mes[0]
-mes = mes.replace(',', '.')
+api_url = 'https://www.imis.bfs.de/ogc/opendata/ows'
+api_request = f'<GetFeature xmlns="http://www.opengis.net/wfs" service="WFS" version="1.1.0" outputFormat="application/json" viewParams="_dc:1674989865629;" xsi:schemaLocation="http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.1.0/wfs.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><Query typeName="odlinfo_odl_1h_latest"><Filter xmlns="http://www.opengis.net/ogc"><PropertyIsEqualTo><PropertyName>plz</PropertyName><Literal>{args.plz}</Literal></PropertyIsEqualTo></Filter></Query></GetFeature>'
 
-mes = float(mes)
+page = requests.post(api_url, data=api_request, headers=headers)
 
-logging.info("Radiation value is: %f" % mes)
+data = page.json()
 
-publish.single(args.mqtt_topic, payload=mes, qos=0, retain=True, hostname=args.mqtt_host,
+number_found = data['numberMatched']
+
+if not number_found:
+    logging.error("0 results found for PLZ %d", args.plz)
+    logging.error(
+        "Select proper station here: https://odlinfo.bfs.de/ODL/DE/themen/wo-stehen-die-sonden/liste/liste_node.html")
+    sys.exit(1)
+
+station = data['features'][0]['properties']
+value = station['value']
+
+logging.debug("Using station: %s (%s)", station['name'], station['plz'])
+logging.info("Value: %f", value)
+
+publish.single(args.mqtt_topic, payload=value, qos=0, retain=True, hostname=args.mqtt_host,
                port=int(args.mqtt_port), client_id="WeatherRadiationMeter", auth=mqtt_auth)
