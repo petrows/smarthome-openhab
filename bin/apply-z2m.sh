b2b341f08f3c811ce14682bb338f1f6539183e67
Petro <petro@petro.ws>
2023-02-11 22:54:48 +0100

    Zigbee2mqtt: backup device config into folder

--- bin/apply-z2m.sh
+++ bin/apply-z2m.sh
@@ -2,7 +2,9 @@
 
 # This script applies devices list to Zigbee2mqtt and reloads it
 
-cp /srv/zigbee2mqtt-data/devices.yaml /srv/zigbee2mqtt-data/devices-$(date +"%Y-%m-%d").yaml
+mkdir -p /srv/zigbee2mqtt-data/devices-backup
+
+cp /srv/zigbee2mqtt-data/devices.yaml /srv/zigbee2mqtt-data/devices-backup/$(date +"%Y-%m-%d").yaml
 cp /srv/openhab-data/conf/devices.yaml /srv/zigbee2mqtt-data/
 
 docker restart Openhab-zigbee2mqtt
