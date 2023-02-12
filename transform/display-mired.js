8afbc6ab0748b58a994c9601e161fdfaf22413ee
Petro <petro@petro.ws>
2023-02-12 22:14:17 +0100

    Cleanup from old files

--- transform/display-mired.js
+++
@@ -1,6 +0,0 @@
-(function (dataString) {
-    // Dsiplay MIRED color temp in K
-    mired = 150 + (500 - 150) * (dataString / 100)
-    kelvin = Math.round(1000000.0 / mired)
-    return kelvin + "K"
-})(input)
