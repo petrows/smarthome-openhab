ea2aabe3fc5e0869c6726eb5b8d630353dbc4d99
Petro <petro@petro.ws>
2024-05-18 11:06:40 +0200

    Disable broken KVV timetable

--- bin/cron-minute.sh
+++ bin/cron-minute.sh
@@ -4,11 +4,12 @@ ROOT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source "$ROOT_PATH/config.sh"
 
-# KVV timetable
+# Timetables broken since 2023
+# # KVV timetable
 
-$ROOT_PATH/kvv-timetable.py --log=INFO --mqtt-host $MQTT_HOST --mqtt-port $MQTT_PORT --mqtt-user $MQTT_USER --mqtt-pass $MQTT_PASS --mqtt-topic kvv-timetable --stop-id $KVV_STOP_ID
+# $ROOT_PATH/kvv-timetable.py --log=INFO --mqtt-host $MQTT_HOST --mqtt-port $MQTT_PORT --mqtt-user $MQTT_USER --mqtt-pass $MQTT_PASS --mqtt-topic kvv-timetable --stop-id $KVV_STOP_ID
 
-# DB timetable
+# # DB timetable
 
-$ROOT_PATH/db-timetable.py --log=INFO --mqtt-host $MQTT_HOST --mqtt-port $MQTT_PORT --mqtt-user $MQTT_USER --mqtt-pass $MQTT_PASS --mqtt-topic db-timetable --stop-id $DB_STOP_ID --api-key $DB_API_KEY
+# $ROOT_PATH/db-timetable.py --log=INFO --mqtt-host $MQTT_HOST --mqtt-port $MQTT_PORT --mqtt-user $MQTT_USER --mqtt-pass $MQTT_PASS --mqtt-topic db-timetable --stop-id $DB_STOP_ID --api-key $DB_API_KEY
 
