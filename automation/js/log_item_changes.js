7ee6049644f543c1566571986004d4019085e39c
Petro <petro@petro.ws>
2026-07-14 22:07:42 +0200

    Add first JS Rule

---
+++ automation/js/log_item_changes.js
@@ -0,0 +1,12 @@
+const { rules, triggers } = require('openhab');
+
+rules.JSRule({
+    name: 'Log all item state changes',
+    id: 'log-all-item-changes',
+    triggers: [
+        triggers.GenericEventTrigger('openhab/items/**', '', 'ItemStateChangedEvent', 'all-item-changes'),
+    ],
+    execute: (event) => {
+        console.info(`${event.itemName}: ${event.payload.oldValue} -> ${event.payload.value}`);
+    },
+});
